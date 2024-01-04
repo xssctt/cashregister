994fc6e439e804f842b7e0c8262af88cb1262cbc
xssctt <2565837829@qq.com>
2024-01-04 16:25:49 +0800

    first commit

---
+++ src/main/java/com/example/cashregister/CashregisterApplication.java
@@ -0,0 +1,13 @@
+package com.example.cashregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashregisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CashregisterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cashregister/CashregisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cashregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashregisterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
