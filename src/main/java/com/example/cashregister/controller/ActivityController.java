b4b0f71a9585a381499563074547615da95722a5
xssctt <2565837829@qq.com>
2024-01-08 16:45:51 +0800

    2

---
+++ src/main/java/com/example/cashregister/controller/ActivityController.java
@@ -0,0 +1,27 @@
+package com.example.cashregister.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/member/activity")
+public class ActivityController {
+
+    /*
+    * POST
+/member/activity/changeStatus
+停用/启用活动
+POST
+/member/activity/create
+新建卡券营销活动
+POST
+/member/activity/delete
+删除活动
+GET
+/member/activity/list
+活动列表
+    * */
+
+
+
+}
---
+++ src/main/java/com/example/cashregister/service/UserService.java
@@ -0,0 +1,4 @@
+package com.example.cashregister.service;
+
+public class UserService {
+}
